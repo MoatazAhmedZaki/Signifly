37e26042a7c591770f6e2a93ca4eb0444d627127
MoatazAhmedZaki <moatazcs@gmailcom>
2021-03-10 10:13:49 +0200

    Updates

---
+++ precache-manifest.05c2d655b03d6e74e65bd7b21cc6ceaf.js
@@ -0,0 +1,182 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71482063455ce89df84a95990ee1a3bc",
+    "url": "/Signifly/index.html"
+  },
+  {
+    "revision": "968c737572878f3220b5",
+    "url": "/Signifly/static/css/2.1a51c785.chunk.css"
+  },
+  {
+    "revision": "2b128faedf1922813641",
+    "url": "/Signifly/static/css/main.4964bb67.chunk.css"
+  },
+  {
+    "revision": "968c737572878f3220b5",
+    "url": "/Signifly/static/js/2.ea201cab.chunk.js"
+  },
+  {
+    "revision": "2b128faedf1922813641",
+    "url": "/Signifly/static/js/main.d71d56db.chunk.js"
+  },
+  {
+    "revision": "ccc59cfd936f174886ae",
+    "url": "/Signifly/static/js/runtime-main.aab0d286.js"
+  },
+  {
+    "revision": "937b4dd560e7ffb6ed054c3193c5c08a",
+    "url": "/Signifly/static/media/1.937b4dd5.svg"
+  },
+  {
+    "revision": "b834beaac6db05acc17f6282074cbe0b",
+    "url": "/Signifly/static/media/2.b834beaa.svg"
+  },
+  {
+    "revision": "75fc81e886534aaee45025eddfc69af3",
+    "url": "/Signifly/static/media/3.75fc81e8.svg"
+  },
+  {
+    "revision": "a568ad61f111908396453325ef943272",
+    "url": "/Signifly/static/media/4.a568ad61.svg"
+  },
+  {
+    "revision": "ad447f3fea2a914752037046743e2ef7",
+    "url": "/Signifly/static/media/5.ad447f3f.svg"
+  },
+  {
+    "revision": "bb2154cb33074603fdf69908b8f696d0",
+    "url": "/Signifly/static/media/6.bb2154cb.svg"
+  },
+  {
+    "revision": "62359730c3b42d1a90e0641b6886b592",
+    "url": "/Signifly/static/media/7.62359730.svg"
+  },
+  {
+    "revision": "87336639a10ad9d62d4130b6f4b65ebd",
+    "url": "/Signifly/static/media/8.87336639.svg"
+  },
+  {
+    "revision": "2aba661e51ccd3c4d9a0cb07d90f3ecf",
+    "url": "/Signifly/static/media/ApprCover.2aba661e.jpg"
+  },
+  {
+    "revision": "b3d3dcbb50f632b2f5d0f4c4b02cc58f",
+    "url": "/Signifly/static/media/WorkCover.b3d3dcbb.jpg"
+  },
+  {
+    "revision": "0075d0effe4988e08c83012dfbfca28a",
+    "url": "/Signifly/static/media/aboutcover.0075d0ef.jpg"
+  },
+  {
+    "revision": "535e56f682d877ea3db8e0f396d0098b",
+    "url": "/Signifly/static/media/alii.535e56f6.PNG"
+  },
+  {
+    "revision": "5c64477ec081513e1f9eaa4a07212c7a",
+    "url": "/Signifly/static/media/energy.5c64477e.svg"
+  },
+  {
+    "revision": "009c6cc362df42c9b0519f0d30210b09",
+    "url": "/Signifly/static/media/fun.009c6cc3.svg"
+  },
+  {
+    "revision": "4306765bc702f84386d93cd8dd6153fb",
+    "url": "/Signifly/static/media/honestly.4306765b.svg"
+  },
+  {
+    "revision": "0ba3f415e4b93f7044b4be5170b2caba",
+    "url": "/Signifly/static/media/img1.0ba3f415.png"
+  },
+  {
+    "revision": "aab109a53b42654d118ce9ff4bab0454",
+    "url": "/Signifly/static/media/img10.aab109a5.png"
+  },
+  {
+    "revision": "df880cbccb9842419c9787942cbe1b57",
+    "url": "/Signifly/static/media/img11.df880cbc.png"
+  },
+  {
+    "revision": "7ac766cacc6facc3d024990fd2ec1098",
+    "url": "/Signifly/static/media/img12.7ac766ca.png"
+  },
+  {
+    "revision": "2fd3d8d599e9af249aec8cf1ddf30c56",
+    "url": "/Signifly/static/media/img2.2fd3d8d5.png"
+  },
+  {
+    "revision": "4cb3307861338506828a5ed30dc0282a",
+    "url": "/Signifly/static/media/img3.4cb33078.png"
+  },
+  {
+    "revision": "8abed89b7b90f45ca970373f57f13cb4",
+    "url": "/Signifly/static/media/img4.8abed89b.png"
+  },
+  {
+    "revision": "8f6a31df4f1b1cfb3620db5337005c05",
+    "url": "/Signifly/static/media/img5.8f6a31df.png"
+  },
+  {
+    "revision": "476ae33eb5bebbf8780dad538a60d50d",
+    "url": "/Signifly/static/media/img6.476ae33e.png"
+  },
+  {
+    "revision": "537db5138efc30fb448b39c803551a5f",
+    "url": "/Signifly/static/media/img7.537db513.png"
+  },
+  {
+    "revision": "99623128cd7be6d61d696291b4ef10df",
+    "url": "/Signifly/static/media/img8.99623128.png"
+  },
+  {
+    "revision": "a85c25c5027b853ff80db0fa211d2b30",
+    "url": "/Signifly/static/media/img9.a85c25c5.png"
+  },
+  {
+    "revision": "d0bd65313a27127de569abfa3e968507",
+    "url": "/Signifly/static/media/poss1.d0bd6531.jpg"
+  },
+  {
+    "revision": "ef117878dbd995a7f532fddb47444dcd",
+    "url": "/Signifly/static/media/poss2.ef117878.jpg"
+  },
+  {
+    "revision": "477e72411d68450b75c1e213153831ca",
+    "url": "/Signifly/static/media/product1.477e7241.png"
+  },
+  {
+    "revision": "c3df0fb41eb968d0e4312c17f8136567",
+    "url": "/Signifly/static/media/robott.c3df0fb4.PNG"
+  },
+  {
+    "revision": "059fddbd799fe1db8af75f795f900dce",
+    "url": "/Signifly/static/media/rocket.059fddbd.PNG"
+  },
+  {
+    "revision": "6671dce919032787baf98a808d7ca57a",
+    "url": "/Signifly/static/media/serr.6671dce9.PNG"
+  },
+  {
+    "revision": "22181e11d162c410ae5b8f1601649bf6",
+    "url": "/Signifly/static/media/serv-card.22181e11.jpg"
+  },
+  {
+    "revision": "36b19d3e4881e5a979fe79b5785f218c",
+    "url": "/Signifly/static/media/servicesCover.36b19d3e.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/Signifly/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/Signifly/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/Signifly/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/Signifly/static/media/slick.f97e3bbf.svg"
+  }
+]);
